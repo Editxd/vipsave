403a9233a8c299fabb4269dff923e1171f38ee2a
Editxd <152770568+Editxd@users.noreply.github.com>
2023-12-04 15:35:55 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -1,9 +1,12 @@
 #ChauhanMahesh/Vasusen/DroneBots/COL
 
-from telethon import TelegramClient
+from pyrogram import Client
+
+from telethon.sessions import StringSession
+from telethon.sync import TelegramClient
+
 from decouple import config
-import logging
-import time
+import logging, time, sys
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
@@ -12,13 +15,37 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 API_ID = config("API_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
 BOT_TOKEN = config("BOT_TOKEN", default=None)
+SESSION = config("SESSION", default=None)
 FORCESUB = config("FORCESUB", default=None)
-ACCESS = int("-1001688331719")
-MONGODB_URI = config("MONGODB_URI", default=None)
-AUTH_USERS = config("AUTH", default=None)
-SUDO_USERS = []
-if len(AUTH_USERS != 0:
-    SUDO_USERS = {int(AUTH_USERS.strip()) for AUTH_USERS in AUTH_USERS.split()}
-else:
-    SUDO_USERS = set()
+AUTH = []
+
+auth = config("AUTH", default=None, cast=int)
+ids = auth.split(",")
+for id in ids:
+    AUTH.append(int(id))
+    
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
+
+userbot = Client(
+    session_name=SESSION, 
+    api_hash=API_HASH, 
+    api_id=API_ID)
+
+try:
+    userbot.start()
+except BaseException:
+    print("Userbot Error ! Have you added SESSION while deploying??")
+    sys.exit(1)
+
+Bot = Client(
+    "SaveRestricted",
+    bot_token=BOT_TOKEN,
+    api_id=int(API_ID),
+    api_hash=API_HASH
+)    
+
+try:
+    Bot.start()
+except Exception as e:
+    print(e)
+    sys.exit(1)
